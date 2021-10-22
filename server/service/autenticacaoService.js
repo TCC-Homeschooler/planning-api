d724ba059815e80501a0d0bec065831bc8541417
Vinicius <vinifladf@gmail.com>
2021-10-22 15:39:15 -0300

    Ajust in code of login

--- server/service/autenticacaoService.js
+++ server/service/autenticacaoService.js
@@ -3,29 +3,28 @@ const usuariosData = require('../data/usuariosData');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.postLogin = function(nome_usuario, senha){
+exports.postLogin = function(nome_usuario, senha, done){
     try{
         const usuario = await usuariosData.getUsuarioPorNomeUsuario(nome_usuario);
-        if(usuario){
-            const senhaValida = await bcrypt.compare(senha, usuario.senha_hash);
-            if(senhaValida){
-                const token = jwt.sign({
-                    id: usuario.id,
-                    nome: usuario.nome,
-                    email: usuario.email,
-                    usuario_tipo: usuario.usuario_tipo
-                },
-                "planning",
-                {
-                    expiresIn: "1d"
-                })
-            }
-        }
-    }
-    catch{
-        response.status(500).json({
-            success: false,
-            message: 'Falha na autenticação' ,
+        if(!usuario){ return done(null, false)}
+        
+        const senhaValida = await bcrypt.compare(senha, usuario.senha_hash);
+        if(!senhaValida){ return done(null, false)}
+        
+        const token = jwt.sign({
+            id: usuario.id,
+            nome: usuario.nome,
+            email: usuario.email,
+            usuario_tipo: usuario.usuario_tipo
+        },
+        "planning",
+        {
+            expiresIn: "1d"
         })
+
+        return done(null, token);
+    }
+    catch(err){
+        done(err, false)
     }
 }
